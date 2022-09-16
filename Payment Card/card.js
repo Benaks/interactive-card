5c723a45b5826d16bb54719920ceb741231c11e5
Ben Akaager <103255013+Benaks@users.noreply.github.com>
2022-09-17 00:58:59 +0100

    Add files via upload

---
+++ Payment Card/card.js
@@ -0,0 +1,44 @@
+  
+   function getVal(){
+   let val = document.getElementById('nameVal').value;
+   document.getElementById('name').innerHTML = val;
+    
+   console.log(val);
+   }
+
+    function getNum(){
+    let num = document.getElementById('numVal').value;
+    document.getElementById('number').innerHTML = num;
+    if(num = ("")){
+       setCustomValidity = ('bla bla bla')
+      
+    }
+    
+    console.log(num);
+    }
+    function getCvc(){
+    let cvc = document.getElementById('cvcVal').value;
+    document.getElementById('cvc').innerHTML = cvc;
+    
+    console.log(cvc);
+    }
+    function getMonth(){
+    let month = document.getElementById('monthVal').value;
+    document.getElementById('month').innerHTML = month ;
+    
+    console.log(month);
+    }
+    function getYear(){
+    let year = document.getElementById('yearVal').value;
+    document.getElementById('year').innerHTML = year;
+    
+    console.log(year);
+    }
+
+    function completePay(){
+    document.getElementById('form').innerHTML
+    console.log(form)
+    
+    }
+
+
